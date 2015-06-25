cbb9e7b2af6d3d9086b2760ef288a3f8a27aad75
deepakbrahmania <daredreamer1990@gmail.com>
2015-06-25 20:15:57 +0530

    Update towerOfHanoi.c

--- towerOfHanoi.c
+++ towerOfHanoi.c
@@ -15,11 +15,11 @@ int main()
   {
     if(disk == 1)
     {
-      printf("%c --> %c",&source_disk,&dest_disk);
+      printf("%c --> %c",source_disk,dest_disk);
       return;
     }
     //move n-1 disks to aux_tower
     TOH(disk-1,source_disk,aux_disk,dest_disk);
-    printf("%c --> %c",&source_disk,&dest_disk");
+    printf("%c --> %c",source_disk,dest_disk);
     TOH(disk-1,aux_disk,dest_disk,source_disk);
   }
